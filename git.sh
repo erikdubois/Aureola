49c8effe6948f9d124ecb21997b9e4b1f475f16c
erikdubois <erik.dubois@gmail.com>
2016-06-26 15:07:16 +0200

    Automatic Backup @ Sun Jun 26 15:07:16 CEST 2016

---
+++ git.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+############################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+############################################################################
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+############################################################################
+# Below command will backup everything inside the project folder
+
+#echo "# Aureola" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git remote add origin https://github.com/erikdubois/Aureola.git
+#git push -u origin master
+
+
+git add --all .
+
+# Committing to the local repository with a message containing the time details
+curtime=$(date)
+git commit -m "Automatic Backup @ $curtime"
+
+# Push the local snapshot to a remote destination
+git push origin master
