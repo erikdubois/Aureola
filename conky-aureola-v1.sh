590f29aa92f7879db6bbc9e2b2650b372a2315ba
Erik Dubois <erik.dubois@gmail.com>
2016-06-29 20:47:12 +0200

    Automatic Backup @ Wed Jun 29 20:47:12 CEST 2016

---
+++ conky-aureola-v1.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+##################################################################################################################
+#
+# Current project : Conky aureola
+#
+# Source 	: 	https://github.com/erikdubois/Aureola
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+########################################
+########        C O N K Y      #########
+########################################
+
+
+
+
+# C O N K Y   A U R E O L A
+# from github
+
+# if there is already a folder, delete or else do nothing
+[ -d /tmp/aureola ] && rm -rf "/tmp/aureola" || echo ""
+# download the github in folder /tmp/aureola
+git clone https://github.com/erikdubois/Aureola /tmp/aureola
+# if there is no hidden folder conky then make one
+[ -d $HOME"/./config/conky" ] || mkdir -p $HOME"/.config/conky"
+# if there is not hidden folder aureola then make one
+# my choice to put all config files in a hidden folder out of side
+[ -d "~/.aureola" ] || mkdir -p $HOME/".aureola"
+# copy all config files to this hidden folder
+cp -r /tmp/aureola/* ~/.aureola
+
+# starting the standard conky so you can see it is working
+cp -r ~/.aureola/sys-info-mono-willemo/conky.conf ~/.config/conky
+# making sure conky is started at boot
+cp start-conky.desktop ~/.config/autostart/start-conky.desktop
+
+#starting the conky 
+conky -c ~/.config/conky/conky.conf
+
+
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
