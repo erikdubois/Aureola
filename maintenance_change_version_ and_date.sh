40f0d2904712f67c7ac29e99b3dda9738bc454c3
Erik Dubois <erik.dubois@gmail.com>
2016-06-29 20:34:33 +0200

    Automatic Backup @ Wed Jun 29 20:34:33 CEST 2016

--- maintenance_change_version_ and_date.sh
+++ maintenance_change_version_ and_date.sh
@@ -1,12 +1,74 @@
-#==============================================================================
-#                               aurora
-# Date    : 26/06/2016
-# Author  : Erik Dubois at http://www.erikdubois.be
-# Version : v1.0.2
-# License : Distributed under the terms of GNU GPL version 2 or later
-# Documentation Dutch: http://erikdubois.be/linux/conky
-#==============================================================================
-
+#!/bin/bash
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+##################################################################################################################
+#
+# Current project : Conky aureola
+#
+# Source 	: 	https://github.com/erikdubois/Aureola
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
 
 find .  -name "conky.conf" -type f -exec sed -i  's/v1.0.3/v1.0.4/g' {} \;
 find .  -name "README.md" -type f -exec sed -i  's/v1.0.3/v1.0.4/g' {} \;
