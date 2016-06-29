8d15add9916169e3201911f1fd0a27db85d07079
Erik Dubois <erik.dubois@gmail.com>
2016-06-29 21:06:11 +0200

    Commit comment : new git on Wed Jun 29 21:06:11 CEST 2016

--- git.sh
+++ git.sh
@@ -79,11 +79,31 @@
 #git push -u origin master
 
 
+# git config --global user.name x
+# git config --global user.email x
+# sudo git config --system core.editor nano
+# git config --global credential.helper cache
+# git config --global credential.helper 'cache --timeout=3600'
+
+
+# Force git to overwrite local files on pull - no merge
+
+# git fetch all
+# git reset --hard orgin/master
+
+
+# Below command will backup everything inside the project folder
 git add --all .
 
-# Committing to the local repository with a message containing the time details
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
 curtime=$(date)
-git commit -m "Automatic Backup @ $curtime"
+git commit -m "Commit comment : $input on $curtime"
 
 # Push the local snapshot to a remote destination
-git push origin master
+git push -u origin master
