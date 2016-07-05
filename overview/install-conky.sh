095e67336b5ab5c1189aa8c0e0fca878179f91a3
Erik Dubois <erik.dubois@gmail.com>
2016-07-05 20:06:49 +0200

    Commit comment : overview on Tue Jul  5 20:06:49 CEST 2016

---
+++ overview/install-conky.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+##################################################################################################################
+#
+# Current project : Conky aureola
+#
+# Source 	: 	https://github.com/erikdubois/Aureola
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+# this little batch file will autostart conky
+# and copy the configuration file to the standard place
+# where conky looks for a configuration file
+# Lua syntax!!
+
+# killing whatever conkies are still working
+killall conky
+sleep 1
+
+# deleting whatever is still in ~/.config/conky/
+
+read -p "Everything in folder ~/.config/conky/ will be deleted. Sure? (y/n)?" choice
+case "$choice" in 
+  y|Y ) rm -r ~/.config/conky/*;;
+  n|N ) echo "Nothing has changed.";;
+  * ) echo "Invalid input.";;
+esac
+
+
+# the standard place conky looks for a config file
+cp * ~/.config/conky/
+# making sure conky is started at boo
+cp start-conky.desktop ~/.config/autostart/start-conky.desktop
+
+#starting the conky again
+conky -c ~/.config/conky/conky.conf
