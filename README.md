7175658d86354179639cb05fb2701d3f5dda8292
Erik Dubois <erik.dubois@gmail.com>
2016-08-17 07:58:56 +0200

    Commit comment : Chip to chip naming in asura on Wed Aug 17 07:58:56 CEST 2016

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Aureola
 
-Version : v1.3.0
+Version : v1.3.1
 
-Date : 17/07/2016
+Date : 17/08/2016
 
 A collection of conky's I made myself and conky's I like that were shared with the community.
 
--- maintenance_change_version_ and_date.sh
+++ maintenance_change_version_ and_date.sh
@@ -70,7 +70,7 @@
 #
 ##################################################################################################################
 
-find .  -name "conky.conf" -type f -exec sed -i  's/v1.2.4/v1.3.0/g' {} \;
-find .  -name "README.md" -type f -exec sed -i   's/v1.2.4/v1.3.0/g' {} \;
-find .  -name "conky.conf" -type f -exec sed -i  's/15\/07\/2016/17\/07\/2016/g' {} \;
-find .  -name "README.md" -type f -exec sed -i   's/15\/07\/2016/17\/07\/2016/g' {} \;
+find .  -name "conky.conf" -type f -exec sed -i  's/v1.3.0/v1.3.1/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   's/v1.3.0/v1.3.1/g' {} \;
+find .  -name "conky.conf" -type f -exec sed -i  's/17\/07\/2016/17\/08\/2016/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   's/17\/07\/2016/17\/08\/2016/g' {} \;
