79c8d05ebe206f6f05404c2b5b4c6a699c3d67b7
Erik Dubois <erik.dubois@gmail.com>
2016-11-11 09:19:44 +0100

    Commit comment :  on Fri Nov 11 09:19:44 CET 2016

--- skeleton/install-conky.sh
+++ skeleton/install-conky.sh
@@ -156,6 +156,48 @@ cp start-conky.desktop ~/.config/autostart/start-conky.desktop
 
 
 
+echo "################################################################" 
+echo "Installing the fonts if you do not have it yet - with choice"
+
+FONT="DejaVuSansMono"
+
+
+if fc-list | grep -i $FONT >/dev/null ; then
+
+	echo "################################################################" 
+    echo "The font is already available. Proceeding ...";
+
+else
+	echo "################################################################" 
+    echo "The font is not currently installed, would you like to install it now? (y/n)";
+    read response
+    if [[ "$response" == [yY] ]]; then
+        echo "Installing the font to the ~/.fonts directory.";
+        cp ~/.config/conky/fonts/* ~/.fonts
+        echo "################################################################" 
+        echo "Building new fonts into the cache files";
+        echo "Depending on the number of fonts, this may take a while..." 
+        fc-cache -fv ~/.fonts
+		echo "################################################################" 
+		echo "Check if the cache build was successful?";    
+        if fc-list | grep -i $FONT >/dev/null; then
+            echo "################################################################" 
+            echo "The font was sucessfully installed!";
+        else
+        	echo "################################################################" 
+            echo "Something went wrong while trying to install the font.";
+        fi
+    else
+    	echo "################################################################" 	
+        echo "Skipping the installation of the font.";
+        echo "Please note that this conky configuration will not work";
+        echo "correctly without the font.";
+    fi
+
+fi
+
+
+
 
 
 ##################################################################################################################
