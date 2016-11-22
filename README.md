2a643f4b3ff010da5c29538a7c42867a80b01811
Erik Dubois <erik.dubois@gmail.com>
2016-11-22 12:23:03 +0100

    Commit comment :  on Tue Nov 22 12:23:03 CET 2016

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Aureola
 
-Version : v1.6.2
+Version : v1.6.3
 
-Date : 20/11/2016
+Date : 22/11/2016
 
 A collection of conky's I made myself and conky's I like that were shared with the community.
 
--- maintenance_change_version_ and_date.sh
+++ maintenance_change_version_ and_date.sh
@@ -70,7 +70,7 @@
 #
 ##################################################################################################################
 
-find .  -name "conky.conf" -type f -exec sed -i  's/v1.6.1/v1.6.2/g' {} \;
-find .  -name "README.md" -type f -exec sed -i   's/v1.6.1/v1.6.2/g' {} \;
-find .  -name "conky.conf" -type f -exec sed -i  's/11\/11\/2016/20\/11\/2016/g' {} \;
-find .  -name "README.md" -type f -exec sed -i   's/11\/11\/2016/20\/11\/2016/g' {} \;
+find .  -name "conky.conf" -type f -exec sed -i  's/v1.6.2/v1.6.3/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   's/v1.6.2/v1.6.3/g' {} \;
+find .  -name "conky.conf" -type f -exec sed -i  's/20\/11\/2016/22\/11\/2016/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   's/20\/11\/2016/22\/11\/2016/g' {} \;
