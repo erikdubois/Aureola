733c2b3f5a15c694c1f1caa2d5f8257aa454316d
Erik Dubois <erik.dubois@gmail.com>
2017-03-14 21:13:55 +0100

    Comment : updated Lazuli and Lazuli8core - correct code for memory on Tue Mar 14 21:13:55 CET 2017

--- maintenance_change_version_and_date.sh
+++ maintenance_change_version_and_date.sh
@@ -8,8 +8,8 @@
 #
 ##################################################################################################################
 
-find .  -name "conky*.conf" -type f -exec sed -i  	's/v1.8.8/v1.8.9/g' {} \;
-find .  -name "README.md" -type f -exec sed -i   	's/v1.8.8/v1.8.9/g' {} \;
-find .  -name "conky.conf" -type f -exec sed -i  	's/08\/03\/2017/13\/03\/2017/g' {} \;
-find .  -name "README.md" -type f -exec sed -i   	's/08\/03\/2017/13\/03\/2017/g' {} \;
+find .  -name "conky*.conf" -type f -exec sed -i  	's/v1.8.9/v1.9.0/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   	's/v1.8.9/v1.9.0/g' {} \;
+find .  -name "conky.conf" -type f -exec sed -i  	's/14\/03\/2017/14\/03\/2017/g' {} \;
+find .  -name "README.md" -type f -exec sed -i   	's/14\/03\/2017/14\/03\/2017/g' {} \;
 
